7b9c4e6358770814359c134a421282af7d8fd701
Daniel Shillcock <daniel.shillcock@me.com>
2021-12-02 09:44:04 +0000

    Update

---
+++ src/SonarSweep.ts
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const __dirname = path.resolve();
+const depths = fs
+    .readFileSync(path.join(__dirname, '..', '2021', 'input.txt'), 'utf-8')
+    .trim()
+    .split('\n')
+    .map((depth) => +depth.trim());
+
+ const result = depths.map((v,i)  => v > depths[i - 1] ).filter(Boolean).length;
+ console.log(`Number of measurements larger than the previous measurement: ${result}`);
