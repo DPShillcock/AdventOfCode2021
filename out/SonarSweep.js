7505b821b5cde151f19001ac19499e5c3020c2c9
Daniel Shillcock <daniel.shillcock@me.com>
2021-12-02 11:23:53 +0000

    Part two

--- out/SonarSweep.js
+++ out/SonarSweep.js
@@ -6,6 +6,24 @@ const values = fs
     .trim()
     .split('\n')
     .map((r) => +r.trim());
+// Part One
 const result = values.map((v, i) => v > values[i - 1]).filter(Boolean).length;
 console.log(`Number of measurements larger than the previous measurement: ${result}`);
+// Part Two
+function Window(arr, size) {
+    let result = [];
+    arr.some((el, i) => {
+        if (i + size >= arr.length)
+            return true;
+        result.push((arr.slice(i, i + size)));
+    });
+    return result;
+}
+const wind = Window(values, 3);
+const sumWindow = [];
+for (let i = 0; i < wind.length; i++) {
+    const windSum = wind[i].reduce((sum, curr) => sum + curr);
+    sumWindow.push(windSum);
+}
+const windowResult = sumWindow.map((v, i) => v > sumWindow[i - 1]).filter(Boolean).length;
 //# sourceMappingURL=SonarSweep.js.map
--- src/SonarSweep.ts
+++ src/SonarSweep.ts
@@ -8,5 +8,25 @@ const values = fs
     .split('\n')
     .map((r) => +r.trim());
 
- const result = values.map((v,i)  => v > values[i-1] ).filter(Boolean).length;
- console.log(`Number of measurements larger than the previous measurement: ${result}`);
+// Part One
+const result = values.map((v, i) => v > values[i - 1]).filter(Boolean).length;
+console.log(`Number of measurements larger than the previous measurement: ${result}`);
+
+// Part Two
+function Window(arr: number[], size: number): number[][] {
+    let result = [];
+    arr.some((el, i) => {
+        if (i + size >= arr.length) return true;
+        result.push((arr.slice(i, i + size)));
+    });
+    return result;
+}
+
+const wind = Window(values, 3);
+const sumWindow = [];
+for (let i = 0; i < wind.length; i++){
+    const windSum = wind[i].reduce((sum, curr) => sum + curr);
+    sumWindow.push(windSum);
+}
+const windowResult = sumWindow.map((v, i) => v > sumWindow[i - 1]).filter(Boolean).length + 1;
+console.log(`Number of windows larger than the previous windows: ${windowResult}`);
