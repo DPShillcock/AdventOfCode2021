1241dd5405ba5d28e4ced2ff65bc8ead87018405
Daniel Shillcock <daniel.shillcock@me.com>
2021-12-02 09:59:31 +0000

    Update... Golf next

--- out/SonarSweep.js
+++ out/SonarSweep.js
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 import * as path from 'path';
 const __dirname = path.resolve();
-const depths = fs
+const values = fs
     .readFileSync(path.join(__dirname, '..', '2021', 'input.txt'), 'utf-8')
     .trim()
     .split('\n')
-    .map((depth) => +depth.trim());
-const result = depths.map((v, i) => v > depths[i - 1]).filter(Boolean).length;
+    .map((r) => +r.trim());
+const result = values.map((v, i) => v > values[i - 1]).filter(Boolean).length;
 console.log(`Number of measurements larger than the previous measurement: ${result}`);
 //# sourceMappingURL=SonarSweep.js.map
